737e6bc4b1b487689674cbb4fec9ef1f1d06004a
shivam18u <44138380+shivam18u@users.noreply.github.com>
2020-11-19 14:47:08 +0530

    Add files via upload

---
+++ upload.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+if [[ ! "$USER" = "hdfs" ]];then 
+	echo "User does not have permission to upload files"
+	echo "Logging in user hdfs"
+	su hdfs
+fi
+EXIST=`hdfs dfs -ls $2 | grep Found`
+
+if [[ $EXIST  ]] ;then
+	echo "Folder exists"
+else
+	echo "Folder doesn't exist"
+	echo "Creating Folder"
+	hdfs dfs -mkdir -p $2
+fi
+
+#compare if files exist and ask for overwrite permission
+
+x=`ls $1`
+y=`hdfs dfs -ls $2| tr -s " " | cut -d " " -f8`
+
+for FILE in $x
+do
+	if [[ `echo $y | grep $FILE` ]];then
+		echo "$FILE Already Exists in destination folder"
+		read -p"Replace $FILE?(Y/N)" replace
+		if [[ "$replace" = "Y" || "$replace" = "y" ]];then
+			hdfs dfs -rm $2/$FILE
+		fi
+	fi
+done
+
+hdfs dfs -put $1 $2;
+
+hdfs dfs -ls $2;
